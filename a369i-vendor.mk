3ca66b0ee17940136a55c746f7bf4a63805ea7f5
Manish4586 <manish.n.manish45@gmail.com>
2018-06-13 18:48:33 +0530

    first commit

---
+++ a369i-vendor.mk
@@ -0,0 +1,12 @@
+
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/a369i/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/a369i/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/a369i/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/a369i/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/a369i/proprietary/xbin,system/xbin)
+
