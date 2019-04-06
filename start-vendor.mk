3569ebde16ab97f7ab983d25ff52dd1391f760d5
melekpro <melekpro6@gmail.com>
2019-04-06 06:32:53 -0400

    Update start-vendor.mk

--- start-vendor.mk
+++ start-vendor.mk
@@ -1,12 +1,12 @@
 
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/start/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/a369i/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/start/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/a369i/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/start/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/a369i/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/start/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/a369i/proprietary/vendor,system/vendor)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mts/start/proprietary/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/a369i/proprietary/xbin,system/xbin)
 
